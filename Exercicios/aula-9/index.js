19f934003ab8eb7968c2a9f754c2b9d47d1f9977
Clécio de Lira Alves <cclecioc.com@gmail.com>
2022-11-20 15:24:28 -0300

    add aula 9

---
+++ Exercicios/aula-9/index.js
@@ -0,0 +1,13 @@
+// Primeira diferença entre VAR e LET
+// ECMAScript 2015 (ES6)
+nome = 'Luiz'; // NÃO FAÇA ISSO
+
+// No VAR é aceito a redeclaração da variável
+var nome = 'Pedro';
+var nome = 'Otávio';
+console.log(nome);
+
+// No LET não é aceito a redeclaração
+let nome1 = 'Clécio';
+let nome1 = 'Lira';
+console.log(nome1);
