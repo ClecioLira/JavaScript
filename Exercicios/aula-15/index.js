2f9fc5eafaf4ef1a27f77ffe77ff0da5e72c5b25
Clécio de Lira Alves <cclecioc.com@gmail.com>
2022-11-26 18:54:34 -0300

    add aula e exercicios

---
+++ Exercicios/aula-15/index.js
@@ -0,0 +1,36 @@
+let num1 = 9.49;
+let num5 = 81;
+
+// arredondar um número
+let num2 = Math.floor(num1);
+console.log(num2);
+
+// arredondar um número a cima
+let num3 = Math.ceil(num1);
+console.log(num3);
+
+// arredondar o número decimal para o número inteiro mais próximo
+let num4 = Math.round(num1);
+console.log(num4);
+
+// achar o maior número dessa sequência
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7));
+
+// achar o menor número dessa sequência
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7));
+
+// gerar um número aleatório entre 0 e 1
+console.log(Math.random());
+
+// gerar um número aleatório
+console.log(Math.round(Math.random() * (10-0) + 0));
+
+// gerar o valor de PI
+console.log(Math.PI);
+
+// potencializar um número
+console.log(Math.pow(2, 10));
+console.log(2 ** 10);
+
+// achar raiz quadrada de um número
+console.log(num5 ** 0.5);
