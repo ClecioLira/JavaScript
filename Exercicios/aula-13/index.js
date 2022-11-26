e11bb704740ddc7cd899f604ef111d35badef067
Clécio de Lira Alves <cclecioc.com@gmail.com>
2022-11-25 21:39:24 -0300

    add aula 13

---
+++ Exercicios/aula-13/index.js
@@ -0,0 +1,33 @@
+//               012 3456789
+let umaString = "Um \"texto\"";
+
+// pesquisar a posiçao do elemento
+console.log(umaString.charAt(4));
+
+// concatenar
+console.log(umaString.concat(' ', 'em um lindo dia.'));
+console.log(umaString + ' em um lindo dia.');
+console.log(`${umaString} em um lindo dia.`);
+
+// saber em qual posição começa o texto
+console.log(umaString.indexOf('texto'));
+console.log(umaString.indexOf('o', 4))
+console.log(umaString.lastIndexOf('m', 4))
+
+// trocar a palavra da string
+console.log(umaString.replace('Um', 'Outro'));
+
+// saber o tamanho da string
+console.log(umaString.length);
+
+// mostrar somente a palavra selecionada
+console.log(umaString.slice(4, 9));
+
+// retirar a palavra de uma string
+console.log(umaString.split('t'));
+
+// deixar a string toda em maiúscula
+console.log(umaString.toUpperCase());
+
+// deixar a string toda em minúscula
+console.log(umaString.toLowerCase());
