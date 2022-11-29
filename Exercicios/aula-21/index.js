0ac092f95cf4b63260f95b1b66f53226f6b34be8
Clécio de Lira Alves <cclecioc.com@gmail.com>
2022-11-29 20:38:59 -0300

    add aula 21

---
+++ Exercicios/aula-21/index.js
@@ -0,0 +1,16 @@
+/**
+ * Operadores de comparação
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual a
+ * == igualdade (valor) NÃO USAR
+ * === igualdade estrita (valor e tipo)
+ * != diferente (valor) NÃO USAR
+ * !== diferente estrito (valor e tipo)
+ */
+
+let num1 = 10;
+let num2 = '10';
+const comp = num1 !== num2;
+console.log(comp);
