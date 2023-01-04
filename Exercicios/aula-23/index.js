32c572f5d520290429b8bce7a035409b8436f74b
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-01-04 20:59:08 -0300

    add aula 23

---
+++ Exercicios/aula-23/index.js
@@ -0,0 +1,13 @@
+/**
+ * && -> false && true == false
+ * || -> false || true == true 
+ * 
+ * FALSY - Valores avaliados em falso
+ * - 0
+ * - ''  ""  ``
+ * - null
+ * - undefined
+ * - NaN
+ */
+
+console.log('clecio' && undefined && 'lira')
