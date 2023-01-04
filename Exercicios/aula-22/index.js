c2187fc1c49eee3ce0af82b8e3a71d4c0f6b5054
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-01-04 20:29:38 -0300

    add

--- Exercicios/aula-22/index.js
+++ Exercicios/aula-22/index.js
@@ -1,7 +1,11 @@
 /**
  * Operadores lógicos
- * && - AND - E
+ * && - AND - E -> todas as expressões precisam ser verdadeiras para retornar true
  * || - OR - OU
  * ! - NOT - NÃO
  */
 
+const exAnd = true && true && false
+const exOr = true || true || false
+console.log(exAnd)
+console.log(exOr)
