a9e62658fbf9d7f86e7cf061b9419f3333dbf60e
Clécio de Lira Alves <cclecioc.com@gmail.com>
2023-01-05 22:31:54 -0300

    add

--- Exercicios/aula-26/assets/js/main.js
+++ Exercicios/aula-26/assets/js/main.js
@@ -1,4 +1,25 @@
-function calcular() {
-    let peso = document.querySelector('#ipeso')
-    let altura = document.querySelector('#ialtura')
+//Capturar evento do submit do formulário
+const form = document.querySelector('#form')
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    const peso = event.target.querySelector('#ipeso')
+    const altura = event.target.querySelector('#ialtura')
+
+    const pesoN = Number.parseFloat(peso.value)
+    const alturaN = Number.parseFloat(altura.value)
+
+    console.log(pesoN, alturaN)
+})
+
+//Criar parágrafo
+function criarP() {
+    const p = document.createElement('p')
+    return p
+}
+
+//Mostrar o resultado
+function setResultado(msg) {
+    const resultado = document.querySelector('#res')
+    resultado.innerHTML = ''
+    const p = criarP()
 }
