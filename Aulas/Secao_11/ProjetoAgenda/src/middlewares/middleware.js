67eaa5df2f7f97bd5466d611ecdc5cfaf43860a7
Clécio de Lira Alves <cclecioc.com@gmail.com>
2024-08-08 15:20:11 -0300

    update agenda

--- Aulas/Secao_11/ProjetoAgenda/src/middlewares/middleware.js
+++ Aulas/Secao_11/ProjetoAgenda/src/middlewares/middleware.js
@@ -20,3 +20,14 @@ exports.csrfMiddleware = (req, res, next) => {
     res.locals.csrfToken = req.csrfToken()
     next()
 }
+
+exports.loginRequired = (req, res, next) => {
+    if(!req.session.user) {
+        req.flash('erros', 'Você precisa fazer o login.')
+        req.session.save(function() {
+            res.redirect('/')
+            return
+        })
+    }
+    next()
+}
