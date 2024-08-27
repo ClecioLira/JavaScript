f017dec4bcedd04bfcfdddcf83712b65a8be9af8
Clécio Lira <cclecioc.com@gmail.com>
2024-08-27 15:58:12 -0300

    update crud api

--- Aulas/Secao_13/API_REST/src/routes/alunoRoutes.js
+++ Aulas/Secao_13/API_REST/src/routes/alunoRoutes.js
@@ -1,8 +1,18 @@
 import {Router} from 'express' // importa o router do express
 import alunoController from '../controllers/AlunoController' // importa a classe aluno controller
+import loginRequired from '../middlewares/loginRequired'
 
 const router = new Router() // cria uma nova rota
 
-router.get('/', alunoController.index) // instancia a rota no metodo get e chama o alunocontroller no metodo index
+router.get('/', alunoController.index)
+
+router.get('/:id', alunoController.show)
+
+router.post('/', loginRequired, alunoController.store)
+
+router.put('/:id', loginRequired, alunoController.update)
+
+router.delete('/:id', loginRequired, alunoController.delete)
+
 
 export default router // exporta a rota
