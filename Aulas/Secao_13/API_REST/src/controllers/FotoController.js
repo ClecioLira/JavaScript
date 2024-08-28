6f0b82b5c2626fa6ee4762405976cd89a772c9cb
Clécio Lira <cclecioc.com@gmail.com>
2024-08-28 08:49:56 -0300

    update api

--- Aulas/Secao_13/API_REST/src/controllers/FotoController.js
+++ Aulas/Secao_13/API_REST/src/controllers/FotoController.js
@@ -1,6 +1,19 @@
+import multer from 'multer'
+import multerConfig from '../config/multerConfig'
+
+const upload = multer(multerConfig).single('foto')
+
 class FotoController { // cria classe
   async store(req, res) {
-    res.json(req.file)
+    return upload(req, res, (erro) => {
+      if(erro) {
+        return res.status(400).json({
+          errors: [erro.code]
+        })
+      }
+
+      return res.json(req.file)
+    })
   }
 }
 
--- Aulas/Secao_13/API_REST/src/routes/fotoRoutes.js
+++ Aulas/Secao_13/API_REST/src/routes/fotoRoutes.js
@@ -1,13 +1,9 @@
 import {Router} from 'express' // importa o router do express
-import multer from 'multer'
 
 import fotoController from '../controllers/FotoController' // importa a classe foto controller
-import multerConfig from '../config/multerConfig'
-
-const upload = multer(multerConfig)
 
 const router = new Router() // cria uma nova rota
 
-router.post('/', upload.single('foto'), fotoController.store)
+router.post('/', fotoController.store)
 
 export default router // exporta a rota
